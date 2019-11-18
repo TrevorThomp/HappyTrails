4a28d9470c90aa6815a6b62c89c376432a750533
Mark Swearingen <markswearingen@bendcable.com>
2019-11-18 14:17:24 -0800

    schema.sql added

---
+++ schema.sql
@@ -0,0 +1,26 @@
+DROP TABLE IF EXISTS locationlist;
+DROP TABLE IF EXISTS trail;
+
+CREATE TABLE trail (
+  id SERIAL PRIMARY KEY,
+  trails_id INT,
+  longitude VARCHAR(255),
+  latitude  VARCHAR(255),
+  name VARCHAR(255),
+  summers VARCHAR(255),
+  difficulty VARCHAR(255),
+  length DECIMAL(4,1),
+  conditionstates TEXT,
+  conditiondetails TEXT,
+  img_small VARCHAR(255),
+  stars DECIMAL(3,1)
+);
+
+CREATE TABLE locationlist (
+  id SERIAL PRIMARY KEY,
+  query_location VARCHAR(255),
+  latitude  VARCHAR(255),
+  longitude  VARCHAR(255),
+  miles_from DECIMAL (4,1),
+  FOREIGN KEY (id) REFERENCES trail(id)
+);
