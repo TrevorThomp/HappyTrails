fa2d33db172f4258b97d88df8cca63c08e15dfa0
Trevor Thompson <Trevor.Ray.Thompson@gmail.com>
2019-11-18 14:13:48 -0800

    completed trail constructor

---
+++ server.js
@@ -0,0 +1,17 @@
+'use strict';
+
+function Trail(data) {
+  this.id = data.id;
+  this.name = data.name;
+  this.summary = data.summary;
+  this.difficulty = data.difficulty;
+  this.stars = data.stars;
+  this.imgSmallMed = data.imgSmallMed;
+  this.latitude = data.latitude;
+  this.longitude = data.longitude;
+  this.length = data.length;
+  this.conditionStatus = data.conditionStatus;
+  this.conditionDetails = data.conditionDetails;
+}
+
+
